ebb7651948232bd9c7562e2b5c3cd3285d1bc4e0
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-03-28 23:12:21 -0500

    added navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,20 @@
+import Nav from "../components/Nav";
 const Home = () => {
-  const authToken = true;
+  const authToken = false;
   const handleClick = () => {
     console.log("clicked");
   };
 
   return (
-    <div className="home">
-      <h1>Swipe Right®</h1>
-      <button className="primary-button" onClick={handleClick}>
-        {authToken ? "Signout" : "Create Account"}
-      </button>
-    </div>
+    <>
+      <Nav />
+      <div className="home">
+        <h1>Swipe Right®</h1>
+        <button className="primary-button" onClick={handleClick}>
+          {authToken ? "Signout" : "Create Account"}
+        </button>
+      </div>
+    </>
   );
 };
 
