15ce6b978117b6e9e1dcb0aa3c58bc694fd77638
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-03-28 22:56:56 -0500

    working on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 const Home = () => {
   const authToken = true;
+  const handleClick = () => {
+    console.log("clicked");
+  };
 
   return (
     <div className="home">
       <h1>Swipe Right®</h1>
-      <button className="primary-button">
+      <button className="primary-button" onClick={handleClick}>
         {authToken ? "Signout" : "Create Account"}
       </button>
     </div>
