aed3ed4e9289252b071995b4825a002a90adab19
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-03-29 23:59:44 -0500

    working on authorization modal

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,21 @@
 import whiteLogo from "../images/Code_Monkey_BW.png"; //placeholder images
 import colorLogo from "../images/Code_Monkey.png";
 
-const Nav = ({ minimal, authToken }) => {
+const Nav = ({ minimal, authToken, setShowModal }) => {
+  const handleClick = () => {
+    setShowModal(true);
+  };
   return (
     <nav>
       <div className="logo-container">
         <img className="logo" src={minimal ? colorLogo : whiteLogo} />
       </div>
 
-      {!authToken && !minimal && <button className="nav-button">Log In</button>}
+      {!authToken && !minimal && (
+        <button className="nav-button" onClick={handleClick}>
+          Log In
+        </button>
+      )}
     </nav>
   );
 };
