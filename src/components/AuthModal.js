f6d2e07df30d0911b65c83de06c8163120cc1650
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-03-29 23:21:56 -0500

    working on authorization modal

---
+++ src/components/AuthModal.js
@@ -0,0 +1,12 @@
+const AuthModal = () => {
+  const handleClick = () => {
+    setShowModal(false);
+  };
+  return (
+    <div>
+      <div onClick={handleClick}>ⓧ</div>
+    </div>
+  );
+};
+
+export default AuthModal;
