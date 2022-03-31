372595ba0c45d96e70c23f5d4962d5370d3e0fe2
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-03-30 23:16:03 -0500

    working on authorization modal

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,11 +1,38 @@
+import { useState } from "react";
+
 const AuthModal = ({ setShowModal }) => {
+  const [email, setEmail] = useState(null);
+
   const handleClick = () => {
     setShowModal(false);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const isSignedUp = true;
+
   return (
     <div className="auth-modal">
-      <div onClick={handleClick}>ⓧ</div>
+      <div className="close-icon" onClick={handleClick}>
+        ⓧ
+      </div>
+      <h2>{isSignedUp ? "CREATE ACCOUNT" : "LOG IN"}</h2>
+      <p>
+        By clicking LOG IN, you agree to our terms. Learn how we process your
+        data in our Privacy Policy and Cookie Policy
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="email"
+          required={true}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </form>
       AUTH MODAL
     </div>
   );
