3235b04e1fd67b283e43c1d109236fb8c379f202
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-03-31 23:47:04 -0500

    working on onboarding page

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -20,18 +20,66 @@ const OnBoarding = () => {
               value={""}
               onChange={handleChange}
             />
-            <label htmlFor="last_name">Last Name</label>
+            <label>Birthday</label>
+            <div className="multi-input-container">
+              <input
+                id="dob_day"
+                type="number"
+                name="dob_day"
+                placeholder="DD"
+                required={true}
+                value={""}
+                onChange={handleChange}
+              />
+              <input
+                id="dob_month"
+                type="number"
+                name="dob_month"
+                placeholder="MM"
+                required={true}
+                value={""}
+                onChange={handleChange}
+              />
+              <input
+                id="dob_year"
+                type="number"
+                name="dob_year"
+                placeholder="YYYY"
+                required={true}
+                value={""}
+                onChange={handleChange}
+              />
+            </div>
+            <label htmlFor="gender">Gender</label>
+            <div className="multi-input-container">
+              <input
+                id="male-gender"
+                type="radio"
+                name="gender_identity"
+                value="male"
+                onChange={handleChange}
+                checked={true}
+              />
+              <label htmlFor="male-gender">Male</label>
+              <input
+                id="female-gender"
+                type="radio"
+                name="gender_identity"
+                value="female"
+                onChange={handleChange}
+                checked={true}
+              />
+              <label htmlFor="female-gender">Female</label>
+            </div>
+            <label htmlFor="show-gender">Show Gender on my profile</label>
             <input
-              id="last_name"
-              type="text"
-              name="last_name"
-              placeholder="last Name"
-              required={true}
-              value={""}
+              id="show-gender"
+              type="checkbox"
+              name="gender_identity"
+              value="female"
               onChange={handleChange}
+              checked={true}
             />
-            <label htmlFor="first_name">First Name</label>
-            <label htmlFor="first_name">First Name</label>
           </section>
         </form>
       </div>
