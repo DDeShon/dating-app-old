87628c5872bee418b6508326b16627b4a6ce5b14
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-04-12 08:48:46 -0500

    working on chat container

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -1,5 +1,16 @@
 const ChatContainer = () => {
-  return <div className="chat-container">Chat Container</div>;
+  return (
+    <div className="chat-container">
+      <ChatHeader />
+
+      <div>
+        <button className="option">Matches</button>
+        <button className="option">Chat</button>
+      </div>
+
+      <MatchesDisplay />
+    </div>
+  );
 };
 
 export default ChatContainer;
